38eb54ebd4dab82b8bf14eba94c7cfcad2b277a7
double J, T <50047351+Powering111@users.noreply.github.com>
2021-11-04 21:50:18 +0900

    yea hahhh

---
+++ Assets/scripts/delete.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class delete : MonoBehaviour
+{
+
+    private bool active=false;
+    public Sprite activeImg, notActiveImg;
+    public void Activate()
+    {
+        active = true;
+
+        gameObject.GetComponent<Image>().sprite = activeImg;
+        gameObject.GetComponent<Button>().onClick.RemoveListener(Activate);
+        gameObject.GetComponent<Button>().onClick.AddListener(Deactivate);
+
+        
+    }
+
+    public void Deactivate()
+    {
+        active = false;
+
+        gameObject.GetComponent<Image>().sprite = notActiveImg;
+        gameObject.GetComponent<Button>().onClick.RemoveListener(Deactivate);
+        gameObject.GetComponent<Button>().onClick.AddListener(Activate);
+    }
+
+    void Update() {
+        if (active && Input.GetMouseButton(0))
+        {
+            Vector2 ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray, Vector2.zero);
+            if (hit.collider != null)
+            {
+                if (hit.transform.parent.tag == "body" || hit.transform.parent.tag == "force")
+                {
+                    Destroy(hit.transform.parent.gameObject);
+                }
+            }
+        }
+    }
+}
